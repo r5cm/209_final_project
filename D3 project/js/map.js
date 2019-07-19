c55c2e1bc97b2e353a2c4ae43f35b7c35fdff7ee
Ramiro Cadavid <ramirocadavid@gmail.com>
2019-07-19 12:23:19 -0500

    Integrated map to index

---
+++ D3 project/js/map.js
@@ -0,0 +1,94 @@
+// Data sources
+var map_link = "https://api.tiles.mapbox.com/v4/mapbox.streets/{z}/{x}/{y}.png?access_token=pk.eyJ1IjoibWFwYm94IiwiYSI6ImNpejY4NXVycTA2emYycXBndHRqcmZ3N3gifQ.rJcFIG214AriISLbB6B5aw";
+var attribution_text = 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, ' + '<a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, ' + 'Imagery © <a href="http://mapbox.com">Mapbox</a>';
+var mm_geodata = "https://raw.githubusercontent.com/r5cm/209_final_project/master/D3%20project/data/test_data.csv"
+var burglar_url = 'https://github.com/r5cm/209_final_project/blob/master/D3%20project/images/bandit-icon-529521.png?raw=true'
+var gun_url = 'https://github.com/r5cm/209_final_project/blob/master/D3%20project/images/icon_gun.png?raw=true'
+
+// Icon variables
+var burglarIcon = L.icon({
+            iconUrl: burglar_url,
+            iconSize:     [14, 30],
+            iconAnchor:   [7, 30],
+            popupAnchor:  [-3, -76]
+        });
+var gunIcon = L.icon({
+            iconUrl: gun_url,
+            iconSize:     [14, 30],
+            iconAnchor:   [7, 30],
+            popupAnchor:  [-3, -76]
+        });
+
+// miscellaneous functions
+function capitalizeFirstLetter(string) {
+  var first = string.charAt(0).toUpperCase();
+  var rest = string.slice(1).toLowerCase();
+  return first + rest
+}
+
+// View without data
+function initializePage() {
+  map = L.map("divmap").setView([37.8782683,-122.259196], 13);
+  L.tileLayer(map_link, {maxZoom: 18, attribution: attribution_text, id: "mapbox.light"})
+    .addTo(map);
+}
+initializePage();
+
+// Add data
+d3.csv(mm_geodata, function(data) {
+    console.log(data);
+  
+    // Get unique crime categories
+    var categories = ['All'];
+    for (var i=0; i < data.length; i++) {
+        var category = data[i]['Category'];
+        if(categories.indexOf(category) < 0) {
+            categories.push(category);
+        };
+    };
+    console.log("Categories:");
+    console.log(categories);
+  
+    // Add crime category menu
+    d3.select("body")
+      .append("select")
+      .attr("id", "crimeSelector")
+      .selectAll("option")
+      .data(categories)
+      .enter()
+      .append("option")
+      .text(d => d)
+      .attr("value", d => d);
+//     var crimeFilter = function(d, selected) {
+        
+//     }
+//     d3.select("#crimeSelector")
+//       .on("change", function(d) {
+//           selected = this['value'];
+//           dateMenuFilter(data, selected);
+//           // main(dataFiltered);
+//         });
+  
+    // Draw markers
+    for(var i=0; i < data.length; i++) {
+        // Get vars
+        var coords = data[i]['LatLng'].replace('(', '').replace(')', '').split(', ');
+        var lat = parseFloat(coords[0]);
+        var long = parseFloat(coords[1]);
+        var date = '<b>Date: </b>' + data[i]['Date']
+        
+        // Create points
+        if (i % 2 == 0) {
+          var point = L.marker([lat, long], {icon: burglarIcon});  
+        } else {
+          var point = L.marker([lat, long], {icon: gunIcon, rotationAngle: 45});  
+        }        
+        
+        //Create popup
+        var cat = '<b>Category: </b>' + capitalizeFirstLetter(data[i]['Category'])
+        var disp = '<b>Disposition: </b>' + capitalizeFirstLetter(data[i]['Disposition'])
+        var pu_content = '<p>'+date+'<br />'+cat+'<br />'+disp+'</p>'
+        point.bindPopup(pu_content)
+             .addTo(map);
+    } 
+});
--- D3 project/js/my-viz.js
+++ D3 project/js/my-viz.js
@@ -1,18 +1,59 @@
-// Shorthand for $( document ).ready()
-$(function() {
-    // let's get started
-    var mysvg = d3.select("#myfigure").append("svg");
+console.log(d3.select(".summary").text());
 
-    mysvg.append("rect")
-        .attr("height",600)
-        .attr("width",600)
-        .attr("fill","black");
+var w = 200;
+var h = 100;
 
-    console.log("Finished drawing rectangle.");
+var dataset = [ 5, 10, 13, 19, 21, 25, 22, 18, 15, 13,
+               11, 12, 15, 20, 18, 17, 16, 18, 23, 25 ];
 
-    d3.text("data/my-data.txt",function(e,d) {
-        console.log("Data loaded. Data:");
-        console.log(d);
-    });
+var xScale = d3.scaleBand()
+  .domain(d3.range(dataset.length))
+  .rangeRound([0, w])
+  .paddingInner(0.05);
+
+var yScale = d3.scaleLinear()
+  .domain([0, d3.max(dataset)])
+  .range([0, h]);
+
+var svg = d3.select(".filter")
+  .append("svg")
+  .attr("width", w)
+	.attr("height", h);
+
+//Create bars
+svg.selectAll("rect")
+  .data(dataset)
+  .enter()
+  .append("rect")
+  .attr("x", function(d, i) {
+  return xScale(i);
+})
+  .attr("y", function(d) {
+  return h - yScale(d);
+})
+  .attr("width", xScale.bandwidth())
+  .attr("height", function(d) {
+  return yScale(d);
+})
+  .attr("fill", function(d) {
+  return "rgb(0, 0, " + Math.round(d * 10) + ")";
 });
 
+//Create labels
+svg.selectAll("text")
+  .data(dataset)
+  .enter()
+  .append("text")
+  .text(function(d) {
+  return d;
+})
+  .attr("text-anchor", "middle")
+  .attr("x", function(d, i) {
+  return xScale(i) + xScale.bandwidth() / 2;
+})
+  .attr("y", function(d) {
+  return h - yScale(d) + 14;
+})
+  .attr("font-family", "sans-serif")
+  .attr("font-size", "11px")
+  .attr("fill", "white");
