03be3cb037e544087040f2c61957eee7342cddb7
smferro54 <sm.ferro54@gmail.com>
2019-07-22 14:43:14 -0400

    Further work on summary table

--- D3 project/js/summary_table.js
+++ D3 project/js/summary_table.js
@@ -1,12 +1,14 @@
 // Summary table from current elements in view.
 
+//Missing the defaultdict counter. Count elements in list. 
+
 function get_visible_data() {
+	//var parser = new DOMParser(); 
 	var visible_data = [];
 	map.eachLayer(function(layer) {
 		if(layer instanceof L.Marker) {
 			if(map.getBounds().contains(layer.getLatLng())) {
-				console.log("element:")
-				console.log(layer._popup._content);
+				console.log(layer._popup._content.match(/<br.*\/>/)[0].match(/\/b>.*</)[0].match(/>.*</)[0].replace(/[><]/g,'')); //Regular expression to get the crime
 			}
 		}
 	});
