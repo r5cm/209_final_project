dc5da1a13a608f3b8e7e80f867d8b4ba7f6206c3
smferro54 <sm.ferro54@gmail.com>
2019-07-27 00:24:11 -0400

    Table works fine and looks good.

--- D3 project/js/summary_table.js
+++ D3 project/js/summary_table.js
@@ -1,17 +1,24 @@
 // Summary table from current elements in view.
 
+invalid_entries = ['UPDATE:']
+
+
 function get_visible_data_summary(){
 	var visible_data = [];
 	map.eachLayer(function(layer) {
 		if(layer instanceof L.Marker) {
 			if(map.getBounds().contains(layer.getLatLng())) {
-				// console.log(layer._popup._content) Fails near James Kenney Park
-				try{
-					cat = layer._popup._content.match(/<br.*\/>/)[0].match(/\/b>.*</)[0].match(/>.*</)[0].replace(/[><]/g,'').replace(/br \//,''); //Regular expression to get the crime.
-					visible_data.push(cat); 
-				}
-				catch(TypeError){
-					cat = layer._popup._content.split(/[t|y]: /)[1].split("<")[0].split("/")[1]; //Regular expression to get the crime from nixle objects, these say advisory/community/alert instead of category.
+				//separate Logs from Nixle:
+				x = layer._popup._content 
+				if(/href/.test(x)){
+					split0 = x.split('<br />').slice(1,this.length-1) // First and last are no good.
+					if(split0[1].length > 0 && invalid_entries.includes(split0[1]) == false){
+						visible_data.push(split0[1]);
+					}else{
+						visible_data.push(split0[0].split(':').slice(-1)[0].replace(' </b>','').trim());
+					}
+				}else{
+					cat = layer._popup._content.match(/<br.*\/>/)[0].match(/\/b>.*</)[0].match(/>.*</)[0].replace(/[><]/g,'').replace(/br \//,'').trim(); //Regular expression to get the crime.
 					visible_data.push(cat);
 				}
 			}
@@ -34,9 +41,9 @@ function get_visible_data_summary(){
 	//console.log(ordered_tuples)
 	ordered_tuples.sort((a,b) => a[1] < b [1]);
 	
-	var html = "<table><tr>";
+	var html = "<table><caption>Most Frequent Events</caption><tr>";
 	for(var i = 0; i < Math.min(ordered_tuples.length,10); i++){
-		html += "<td>" + ordered_tuples[i][0] + "<td>" + ordered_tuples[i][1] + "</td></tr><tr>";
+		html += "<td>" + ordered_tuples[i][0] + '<td class="count">' + ordered_tuples[i][1] + "</td></tr><tr>";
 	}
 	html += "</tr></table>";
 	
